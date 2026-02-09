370a9b4e2c91faac0cf39e6ed51955a54a0c5e80
matejajelisavcic <matejajelisavcic@gmail.com>
2026-02-09 14:40:43 +0000

    feb902

--- feb9/app.py
+++ feb9/app.py
@@ -21,14 +21,20 @@ b = torch.tensor([
 ], requires_grad=True)
 
 lr = .01
-for i in range(1000):
+for i in range(50000):
     Yhat = X@w+b
     r = Yhat-Y
     SSE = r.T@r
     loss = SSE/3
+
     loss.backward()
     with torch.no_grad():
         w -= lr*w.grad
         b -= lr*b.grad
-    print(w, b)
+    print(loss.item(), w, b)
 
+    w.grad.zero_()
+    b.grad.zero_()
+
+print(7.0*w+b)
+# -0.3723 6.8561
