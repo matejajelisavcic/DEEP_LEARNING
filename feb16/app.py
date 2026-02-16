92245c1efa1885845e14f52caba013567142aaae
matejajelisavcic <matejajelisavcic@gmail.com>
2026-02-16 14:21:23 +0000

    feb1601

--- feb16/app.py
+++ feb16/app.py
@@ -5,3 +5,15 @@ import torch.nn as nn
 import torch.optim as optim
 
 data = pd.read_csv('data.csv')
+
+features = torch.tensor(data.drop('Price',axis=1).to_numpy()).float()
+target = torch.tensor(data['Price'].to_numpy()).float().reshape(-1,1)
+
+fm = features.mean()
+fs = features.std()
+tm = target.mean()
+ts = target.std()
+
+
+
+print(fm)
